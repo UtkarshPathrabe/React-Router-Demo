3b223f133054cd837af0a7b1ee8e0f6928bbd81c
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-02-05 21:42:08 +0530

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    const navLinkStyles = ({ isActive }) => {
+        return {
+            fontWeight: isActive ? 'bold' : 'normal',
+            textDecoration: 'none',
+            fontSize: '1.5rem'
+        };
+    };
+    return (
+        <nav>
+            <NavLink style={navLinkStyles} to='/'>Home</NavLink>
+            <NavLink style={navLinkStyles} to='/about'>About</NavLink>
+        </nav>
+    );
+};
+
+export default Navbar;
