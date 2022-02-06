85ab4e9a608b77e146895a96bbfd6d705feca59a
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-02-06 22:58:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import NoMatch from './components/NoMatch';
+import OrderSummary from './components/OrderSummary';
 
 const App = () => {
 	return (
@@ -10,6 +12,8 @@ const App = () => {
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='about' element={<About />} />
+				<Route path='order-summary' element={<OrderSummary />} />
+				<Route path='*' element={<NoMatch />} />
 			</Routes>
 		</>
 	);
