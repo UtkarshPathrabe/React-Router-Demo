b05f68cb3396eb88d3acdbe122e22798ed00efb7
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-02-06 22:58:41 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+    const navigate = useNavigate();
     return (
-        <div style={{ fontSize: '2rem' }}>Home Page</div>
+        <>
+            <div  style={{ height: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '2rem', width: '100%' }}>
+                Home Page
+            </div>
+            <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+                <button onClick={() => navigate('order-summary')}>Place Order</button>
+            </div>
+        </>
     );
 };
 
