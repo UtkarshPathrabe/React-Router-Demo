2ec7435dda7f0e242c0951ac9ac8bf61ef1d10d7
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-02-19 20:56:59 +0530

    Create NewProducts.js

---
+++ src/components/NewProducts.js
@@ -0,0 +1,9 @@
+const NewProducts = () => {
+    return (
+        <div style={{ width: "100%", height: "60vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            List of New Products
+        </div>
+    );
+};
+
+export default NewProducts;
