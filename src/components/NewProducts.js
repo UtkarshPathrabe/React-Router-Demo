57c9bb822552902a6d0baf0bb29682f4538215a1
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-02-19 21:00:03 +0530

    Update NewProducts.js

--- src/components/NewProducts.js
+++ src/components/NewProducts.js
@@ -1,6 +1,6 @@
 const NewProducts = () => {
     return (
-        <div style={{ width: "100%", height: "60vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <div style={{ width: "100%", height: "60vh", display: "flex", alignItems: "center", justifyContent: "center", fontSize: '1rem' }}>
             List of New Products
         </div>
     );
