f49bb46a21f7ca276045f931d48a8b73a30c2d42
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-02-19 20:56:55 +0530

    Create FeaturedProducts.js

---
+++ src/components/FeaturedProducts.js
@@ -0,0 +1,9 @@
+const FeaturedProducts = () => {
+    return (
+        <div style={{ width: "100%", height: "60vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            List of Featured Products
+        </div>
+    );
+};
+
+export default FeaturedProducts;
