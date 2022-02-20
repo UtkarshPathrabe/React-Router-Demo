a2214630306eddfb948946bdacdac9c415d9ccc0
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-02-20 10:58:34 +0530

    Delete NewProducts.js

--- src/components/NewProducts.js
+++
@@ -1,9 +0,0 @@
-const NewProducts = () => {
-    return (
-        <div style={{ width: "100%", height: "60vh", display: "flex", alignItems: "center", justifyContent: "center", fontSize: '1rem' }}>
-            List of New Products
-        </div>
-    );
-};
-
-export default NewProducts;
