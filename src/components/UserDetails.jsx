bec1b2b80f611a4cf8993090b0f7ca163b55aeae
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-02-20 11:57:23 +0530

    Update UserDetails.jsx

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 const UserDetails = () => {
     const { userId } = useParams();
     return (
-        <div>
+        <div style={{ width: "100%", margin: "1rem" }}>
             Details about user {userId}
         </div>
     );
