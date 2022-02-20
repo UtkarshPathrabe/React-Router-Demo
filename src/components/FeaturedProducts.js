4e2c938c4418073ff95f96c3b3818d6b1da215c8
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-02-20 10:58:25 +0530

    Delete FeaturedProducts.js

--- src/components/FeaturedProducts.js
+++
@@ -1,9 +0,0 @@
-const FeaturedProducts = () => {
-    return (
-        <div style={{ width: "100%", height: "60vh", display: "flex", alignItems: "center", justifyContent: "center", fontSize: '1rem' }}>
-            List of Featured Products
-        </div>
-    );
-};
-
-export default FeaturedProducts;
