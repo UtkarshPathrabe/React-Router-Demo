ff1c78e8a619b3b1fc0ef0c6604a466c0a2a86a4
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-02-20 10:58:36 +0530

    Create NewProducts.jsx

---
+++ src/components/NewProducts.jsx
@@ -0,0 +1,9 @@
+const NewProducts = () => {
+    return (
+        <div style={{ width: "100%", height: "60vh", display: "flex", alignItems: "center", justifyContent: "center", fontSize: '1rem' }}>
+            List of New Products
+        </div>
+    );
+};
+
+export default NewProducts;
