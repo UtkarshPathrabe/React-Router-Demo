d133e3802d3f8a21470f390fe5c2f42b4cb4e1e7
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-02-20 12:58:58 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import { useAuth } from "./auth";
+import { useAuth } from "../utilities/auth";
 
 const Login = () => {
     const [user, setUser] = useState('');
